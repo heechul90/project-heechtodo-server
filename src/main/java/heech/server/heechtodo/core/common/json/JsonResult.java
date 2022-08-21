470d13edb9c490f5d76d5303dd73f039447fbb5b
hclee <hclee@dataeum.kr>
2022-08-21 18:16:11 +0900

    core > common > json 추가

---
+++ src/main/java/heech/server/heechtodo/core/common/json/JsonResult.java
@@ -0,0 +1,45 @@
+package heech.server.heechtodo.core.common.json;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+public class JsonResult<T> {
+
+    private LocalDateTime transaction_time;
+    private HttpStatus status;
+    private String message = "";
+
+    private List<Error> errors;
+    private T data;
+
+    public static <T> JsonResult<T> OK() {
+        return (JsonResult<T>) JsonResult.builder()
+                .transaction_time(LocalDateTime.now())
+                .status(HttpStatus.OK)
+                .build();
+    }
+
+    public static <T> JsonResult<T> OK(T data) {
+        return (JsonResult<T>) JsonResult.builder()
+                .transaction_time(LocalDateTime.now())
+                .status(HttpStatus.OK)
+                .data(data)
+                .build();
+    }
+
+    public static <T> JsonResult<T> ERROR(HttpStatus status, String message, List<Error> errors) {
+        return (JsonResult<T>) JsonResult.builder()
+                .transaction_time(LocalDateTime.now())
+                .status(status)
+                .message(message)
+                .errors(errors)
+                .build();
+    }
+
+}
