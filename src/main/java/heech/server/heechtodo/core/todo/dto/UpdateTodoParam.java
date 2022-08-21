cb8cbc221b70389802e3bc701005e81ec2fca54f
hclee <hclee@dataeum.kr>
2022-08-21 19:08:46 +0900

    todo > dto 수정

--- src/main/java/heech/server/heechtodo/core/todo/dto/UpdateTodoParam.java
+++ src/main/java/heech/server/heechtodo/core/todo/dto/UpdateTodoParam.java
@@ -1,4 +1,14 @@
 package heech.server.heechtodo.core.todo.dto;
 
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 public class UpdateTodoParam {
+
+    private String title;
+    private int order;
+    private boolean completed;
 }
