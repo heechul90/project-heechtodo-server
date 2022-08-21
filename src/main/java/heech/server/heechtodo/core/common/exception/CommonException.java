9f3ee5eafefb3207116bfe5209c723e7a4c34e13
hclee <hclee@dataeum.kr>
2022-08-21 18:16:33 +0900

    core > common > exception 추가

---
+++ src/main/java/heech/server/heechtodo/core/common/exception/CommonException.java
@@ -0,0 +1,28 @@
+package heech.server.heechtodo.core.common.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public abstract class CommonException extends RuntimeException {
+
+    List<Error> errors = new ArrayList<>();
+
+    public CommonException(String message) {
+        super(message);
+    }
+
+    public CommonException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public abstract HttpStatus status();
+
+    public void addError(List<Error> errors) {
+        this.errors = errors;
+    }
+
+}
